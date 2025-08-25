8a1d22d78990d0cf2bf072d2ef80b3b5583cee0e
barney <tinodeclascaballer@gmail.com>
2025-08-25 19:30:52 -0300

    exercise 1 done

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,14 +1,1 @@
-def suma(a,b):
-    return a+b
-print(suma(5,6,))
-
-def suma_args(*args):
-    return sum(args)
-
-def suma_args(*args):
-
-    # funciona de la misma manera
-    total = 0
-    for arg in args:
-        total += arg
-    return total
+print("Hola, soy Valentino, y este es mi primer codigo en Python")
